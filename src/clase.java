162c258052d0aef1ee50bda68021c6d86d0e8bd5
DAM <a@a.a>
2023-09-26 09:28:17 +0200

    Añadida clase "clase"

---
+++ src/clase.java
@@ -0,0 +1,15 @@
+public class clase {
+    int id;
+
+    public clase(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
